a42e97c3285bfb852f84bc447562985cfac13430
emaansajjad284-alt <emaansajjad284@gmail.com>
2026-04-23 19:53:13 +0500

    Add files via upload
    Initial commit: Added portfolio website files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Jab menu links par click ho toh wo smoothly scroll karke section par jaye
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
